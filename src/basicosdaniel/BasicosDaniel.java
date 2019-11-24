bdc8c53681e9644e6d899e17987ff9a3a8af92cf
Daniel Guardia <daniguardiamail@gmail.com>
2019-11-24 23:10:05 +0100

    finalizada la rama cadenas

--- src/basicosdaniel/BasicosDaniel.java
+++ src/basicosdaniel/BasicosDaniel.java
@@ -3,9 +3,22 @@ package basicosdaniel;
 //@author Daniel Guardia Marintez
  
 public class BasicosDaniel {
-
+    
+    public static void cadenas(){
+        char caracter;
+        String cadena;
+        
+        cadena = "Daniel Guardia";
+        caracter = 'M';
+        
+        System.out.println("Valor String " + cadena);
+        System.out.println("Valor char " + caracter);
+    }
+    
     public static void main(String[] args) {
-        //
+        
+        cadenas();
+        
         
     }
     
