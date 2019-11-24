61d0d2d900e86e7a59484a6b66f78dd50a4b7ae7
Daniel Guardia <daniguardiamail@gmail.com>
2019-11-24 23:24:05 +0100

    completado la rama logicos

--- src/basicosdaniel/BasicosDaniel.java
+++ src/basicosdaniel/BasicosDaniel.java
@@ -4,8 +4,24 @@ package basicosdaniel;
  
 public class BasicosDaniel {
 
+    public static void logicos(){
+         boolean logico;
+         
+         logico = true;
+         
+         System.out.print ("Valor boolean " );
+         if (logico) 
+            System.out.println("verdadero");
+         else
+            System.out.println("falso");
+         
+        }
+    
     public static void main(String[] args) {
-        //
+        
+        logicos();
+        
+        
         
     }
     
